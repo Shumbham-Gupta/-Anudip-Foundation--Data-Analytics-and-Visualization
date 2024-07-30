dc9e136731fcaff360beea0f6adc718a12d0d613
Shumbham-Gupta <144575875+Shumbham-Gupta@users.noreply.github.com>
2024-07-30 14:26:38 +0530

    Add files via upload

---
+++ LAB1_Assignment.py
@@ -0,0 +1,30 @@
+# import numpy as np
+
+# question - 1
+# my_list = [1,2,3,4,5]
+# print("NumPy Array : ",np.array(my_list))
+
+
+# question - 2
+# list = [1,2,3,4,5]
+# mat = np.array(list)
+# print(mat)
+# print("First Element: " , mat[0])
+# print("Last Element : " , mat[-1])
+# mat *= 2
+# print("After doubling each element: ",mat)
+
+
+# question - 3
+# zero = np.zeros(10)
+# print("Array of 10 zeros : ",zero)
+# one = np.ones(10)
+# print("Array of 10 ones : ",one)
+# five = np.full(10,5.)
+# print("Array of 10 fives : ",five)
+
+
+# question - 4
+# print("Numpy Array To Create 3*3 matrix")
+# mat = np.arange(2,11)
+# print(mat.reshape(3,3))
